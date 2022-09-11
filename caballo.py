f0fb93ca0ff0c7e72dd84bf74100052cd1c3298b
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-11 19:40:39 +0200

    gg

--- caballo.py
+++ caballo.py
@@ -23,11 +23,10 @@ def casillas_caballo3():
 
 print(casillas_caballo3())
 
-def casillas_caballo(cantidad):
-    list=[]
-    if cantidad>1:
-        for i in range(len(movs)):
-            
+movs2={"0":["4","6"],"1":["6","8"],"2":["7","9"],"3":["4","8"],"4":["3","9","0"],"5":[],"6":["1","7","0"],"7":["2","6"],"8":["1","3"],"9":["2","4"]}
+
+
+
 
 
 
